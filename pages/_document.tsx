e62a8791cdd098d6459d847082640089360c935d
xueyong <1085523029@qq.com>
2022-11-20 16:40:10 +0800

    💄 add wenkai font

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/lxgw-wenkai-lite-webfont@1.6.0/style.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
